a1d616847257bd83ef6a9a5973bf2b090dafcbbf
tongwen <602943077@qq.com>
2018-03-13 14:17:46 +0800

    修改定时任务中的弹出用户的部分

--- cron_redis.php
+++ cron_redis.php
@@ -7,17 +7,17 @@
  */
 $redis = new Redis();
 $redis->connect('127.0.0.1',6379);
-// 使用一个计划任务
-// 1.写一个堵塞的程序
+//// 使用一个计划任务
+//// 1.写一个堵塞的程序
 $user = $redis->brPop('user',59);
 
-$re = $redis->sIsMember('result',$user);
+$re = $redis->sIsMember('result',$user[1]);
 if($re){    // 已抢单过
 //    echo '已经抢过单了';
 }else{  // 还没有抢单
    $store = $redis->rPop('store');
     if($store){
-        $redis->sAdd('result',$user);
+        $redis->sAdd('result',$user[1]);
         // 做数据库的操作
 //        echo '抢单成功';
     }else{
