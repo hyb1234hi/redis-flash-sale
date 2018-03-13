cb43051b31b45bef469c654cbec13823e0ff1c67
tongwen <602943077@qq.com>
2018-03-13 14:24:07 +0800

    修改定时任务的判断用户的地方

--- cron_redis.php
+++ cron_redis.php
@@ -10,18 +10,19 @@ $redis->connect('127.0.0.1',6379);
 //// 使用一个计划任务
 //// 1.写一个堵塞的程序
 $user = $redis->brPop('user',59);
-
-$re = $redis->sIsMember('result',$user[1]);
-if($re){    // 已抢单过
+if(!empty($user)){
+    $re = $redis->sIsMember('result',$user[1]);
+    if($re){    // 已抢单过
 //    echo '已经抢过单了';
-}else{  // 还没有抢单
-   $store = $redis->rPop('store');
-    if($store){
-        $redis->sAdd('result',$user[1]);
-        // 做数据库的操作
+    }else{  // 还没有抢单
+        $store = $redis->rPop('store');
+        if($store){
+            $redis->sAdd('result',$user[1]);
+            // 做数据库的操作
 //        echo '抢单成功';
-    }else{
-        // 没有库存了
+        }else{
+            // 没有库存了
 //        echo '没有库存了';
+        }
     }
 }
