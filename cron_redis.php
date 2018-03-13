762d3f0700770dfa905e44333390374eb333a94b
tongwen <602943077@qq.com>
2018-03-13 14:05:05 +0800

    修改用户为随机用户

--- cron_redis.php
+++ cron_redis.php
@@ -9,19 +9,20 @@ $redis = new Redis();
 $redis->connect('127.0.0.1',6379);
 // 使用一个计划任务
 // 1.写一个堵塞的程序
-$user = $redis->rPop('user',10);
+$rand = rand(0,1000);
+$user = $redis->rPop('user',$rand);
 
 $re = $redis->sIsMember('result',$user);
 if($re){    // 已抢单过
-    echo '已经抢过单了';
+//    echo '已经抢过单了';
 }else{  // 还没有抢单
    $store = $redis->rPop('store');
     if($store){
         $redis->sAdd('result',$user);
         // 做数据库的操作
-        echo '抢单成功';
+//        echo '抢单成功';
     }else{
         // 没有库存了
-        echo '没有库存了';
+//        echo '没有库存了';
     }
 }
